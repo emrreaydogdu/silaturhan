76305caaf6e70be5b82327e243402feebbf92291
emrreayydogdu-cloud <emrreayydogdu@gmail.com>
2026-08-31 23:49:19 +0300

    Add site build output

---
+++ dist/server/__vite_rsc_assets_manifest.js
@@ -0,0 +1,110 @@
+export default {
+  "bootstrapScriptContent": "import(\"/_next/static/chunks/index-D1j3vLB1.js\")",
+  "clientReferenceDeps": {
+    "897d4ae24f25": {
+      "js": [
+        "/_next/static/chunks/appointment-booking-9CIoC0ma.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/Icon-D0zGVybZ.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js",
+        "/_next/static/chunks/index-D1j3vLB1.js"
+      ],
+      "css": []
+    },
+    "7fbcb3ab3c90": {
+      "js": [
+        "/_next/static/chunks/entry-experience-MYJmJGQ8.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js",
+        "/_next/static/chunks/index-D1j3vLB1.js"
+      ],
+      "css": []
+    },
+    "4715d14f82e4": {
+      "js": [
+        "/_next/static/chunks/site-chrome-B6pmjcOc.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/appointment-booking-9CIoC0ma.js",
+        "/_next/static/chunks/Icon-D0zGVybZ.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js",
+        "/_next/static/chunks/index-D1j3vLB1.js"
+      ],
+      "css": []
+    },
+    "339f946bc456": {
+      "js": [
+        "/_next/static/chunks/Icon-D0zGVybZ.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js",
+        "/_next/static/chunks/index-D1j3vLB1.js"
+      ],
+      "css": []
+    },
+    "9276801271d6": {
+      "js": [
+        "/_next/static/chunks/index-D1j3vLB1.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js"
+      ],
+      "css": []
+    },
+    "0b874ad30386": {
+      "js": [
+        "/_next/static/chunks/index-D1j3vLB1.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js"
+      ],
+      "css": []
+    },
+    "593f344dc510": {
+      "js": [
+        "/_next/static/chunks/index-D1j3vLB1.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js"
+      ],
+      "css": []
+    },
+    "bad85346fc72": {
+      "js": [
+        "/_next/static/chunks/index-D1j3vLB1.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js"
+      ],
+      "css": []
+    },
+    "15c18cfaeeff": {
+      "js": [
+        "/_next/static/chunks/layout-segment-context-D115Phlv.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/index-D1j3vLB1.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js"
+      ],
+      "css": []
+    },
+    "8c0f216c4604": {
+      "js": [
+        "/_next/static/chunks/index-D1j3vLB1.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js"
+      ],
+      "css": []
+    },
+    "89c3cac48cb5": {
+      "js": [
+        "/_next/static/chunks/streamed-icons-B7zI55ff.js",
+        "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
+        "/_next/static/chunks/framework-D_rUT4EX.js",
+        "/_next/static/chunks/index-D1j3vLB1.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/_next/static/css/index.C91Two5O.css"
+      ]
+    }
+  }
+}
